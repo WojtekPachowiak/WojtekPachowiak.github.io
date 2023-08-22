86fd42911a57a5f83294e0e90c4d34c5c4a58183
Wojtek Pachowiak <wojtekpaszo@gmail.com>
2023-08-22 21:45:26 +0200

    alert

--- www/index.js
+++ www/index.js
@@ -19,6 +19,12 @@ import WebGL from "three/addons/capabilities/WebGL.js";
 
 import headersFont from "/resources/fonts/helvetiker_regular.typeface.json?url";
 
+// if mobile device
+if (matchMedia("(pointer:coarse)").matches) {
+  alert("Mobile devices not supported yet. Sorry! :(")
+  throw "Mobile devices not supported yet. Sorry! :(";
+}
+
 if (WebGL.isWebGL2Available() === false) {
   document.body.appendChild(WebGL.getWebGL2ErrorMessage());
 }
