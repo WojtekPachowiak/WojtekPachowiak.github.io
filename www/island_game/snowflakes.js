e86152564a9a217bfe0c084f20150ad806479492
Wojtek Pachowiak <wojtekpaszo@gmail.com>
2024-03-12 22:10:52 +0100

    snowflakes with rapier

--- www/island_game/snowflakes.js
+++ www/island_game/snowflakes.js
@@ -52,7 +52,7 @@ export function snowflakesInit() {
     );
   }
 
-g.SNOWFLAKES.GEOMETRY = new THREE.BufferGeometry();
+  g.SNOWFLAKES.GEOMETRY = new THREE.BufferGeometry();
   g.SNOWFLAKES.GEOMETRY.setAttribute(
     "position",
     new THREE.Float32BufferAttribute(g.SNOWFLAKES.PARTICLES_POSITIONS, 3)
@@ -66,7 +66,10 @@ g.SNOWFLAKES.GEOMETRY = new THREE.BufferGeometry();
     new THREE.Float32BufferAttribute(new Array(g.SNOWFLAKES.NUM).fill(1), 1)
   );
 
-  const snowflakesPoints = new THREE.Points(g.SNOWFLAKES.GEOMETRY, snowflakeMaterial);
+  const snowflakesPoints = new THREE.Points(
+    g.SNOWFLAKES.GEOMETRY,
+    snowflakeMaterial
+  );
   snowflakesPoints.frustumCulled = false;
   // snowflakesPoints.layers.set(g.LAYERS.SNOWFLAKES);
   g.SCENE.add(snowflakesPoints);
@@ -84,7 +87,8 @@ function snowflakesGetPositions() {
       concentrationAroundPlayer +
       g.CAMERA.position.x +
       g.PLAYER.VELOCITY.x * playerVelocityOffset,
-    (Math.random() * 0.2 + 0.9) * g.SNOWFLAKES.SPAWN_HEIGHT + g.CAMERA.position.y,
+    (Math.random() * 0.2 + 0.9) * g.SNOWFLAKES.SPAWN_HEIGHT +
+      g.CAMERA.position.y,
     (Math.random() - 0.5) *
       spread *
       g.SNOWFLAKES.SPAWN_RADIUS *
@@ -102,8 +106,39 @@ function snowflakesGetVelocities() {
   ];
 }
 
+
+function updateDeadSnowflakes(i, deltaTime) {
+  // zero out velocity if hit floor and set to die
+  g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3] = 0;
+  g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 1] = 0;
+  g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 2] = 0;
+  g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] -=
+    deltaTime / g.SNOWFLAKES.DYING_DURATION;
+  if (g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] < 0) {
+    g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] = 0;
+
+    // reset position and velocity (offset position by players velocity)
+    const position = snowflakesGetPositions();
+    g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3] = position[0];
+    g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 1] = position[1];
+    g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 2] = position[2];
+    const velocity = snowflakesGetVelocities();
+    g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3] = velocity[0];
+    g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 1] = velocity[1];
+    g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 2] = velocity[2];
+    g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] = 1;
+  }
+}
+
 export function snowflakesUpdate(deltaTime) {
   for (let i = 0; i < g.SNOWFLAKES.NUM; i++) {
+
+    // if snowflake is dead
+    if (g.SNOWFLAKES.GEOMETRY.attributes.lifetime[i] < 1) {
+      updateDeadSnowflakes(i, deltaTime);
+      continue;
+    }
+
     // update positions
     g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3] +=
       g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3] * deltaTime;
@@ -122,30 +157,31 @@ export function snowflakesUpdate(deltaTime) {
     );
 
     // if snowflake is below ground, stop it and let it die
-    const result = g.OCTREE.sphereIntersect(
-      g.SNOWFLAKES.PARTICLES_COLLISION_SPHERES[i]
-    );
-    if (result) {
-      // zero out velocity if hit floor and set to die
-      g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3] = 0;
-      g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 1] = 0;
-      g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 2] = 0;
-      g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] -=
-        deltaTime / g.SNOWFLAKES.DYING_DURATION;
-      if (g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] < 0) {
-        g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] = 0;
+    // const result = g.OCTREE.sphereIntersect(
+    //   g.SNOWFLAKES.PARTICLES_COLLISION_SPHERES[i]
+    // );
+    const shape = new g.PHYSICS.RAPIER.Ball(0.5);
+    // position of the snowflake
+    const shapePos = {
+      x: g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3],
+      y: g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 1],
+      z: g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 2],
+    };
+    const shapeRot = { w: 1.0, x: 0.0, y: 0.0, z: 0.0 };
 
-        // reset position and velocity (offset position by players velocity)
-        const position = snowflakesGetPositions();
-        g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3] = position[0];
-        g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 1] = position[1];
-        g.SNOWFLAKES.GEOMETRY.attributes.position.array[i * 3 + 2] = position[2];
-        const velocity = snowflakesGetVelocities();
-        g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3] = velocity[0];
-        g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 1] = velocity[1];
-        g.SNOWFLAKES.GEOMETRY.attributes.velocity.array[i * 3 + 2] = velocity[2];
-        g.SNOWFLAKES.GEOMETRY.attributes.lifetime.array[i] = 1;
+    let result = false;
+    g.PHYSICS.WORLD.intersectionsWithShape(
+      shapePos,
+      shapeRot,
+      shape,
+      (handle) => {
+        // console.log("The collider", handle, "intersects our shape.");
+        result = true; // Return `false` instead if we want to stop searching for other colliders that contain this point.
       }
+    );
+
+    if (result) {
+      updateDeadSnowflakes(i, deltaTime);
     }
   }
 
